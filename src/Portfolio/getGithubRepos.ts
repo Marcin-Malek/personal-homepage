221870506cef37f6984152d3a5f342f5d0114d6d
Marcin Malek <marcinmalek01@gmail.com>
2022-11-01 16:39:41 +0100

    added repositories sorting, fixed problem with styled-components module file

--- src/Portfolio/getGithubRepos.ts
+++ src/Portfolio/getGithubRepos.ts
@@ -1,5 +1,10 @@
 import axios from "axios";
 
+const params = new URLSearchParams();
+params.append("sort", "created");
+
+const reposEndpoint = `https://api.github.com/users/Marcin-Malek/repos?${params.toString()}`;
+
 export const getGithubRepos = () => axios
-    .get("https://api.github.com/users/Marcin-Malek/repos")
+    .get(reposEndpoint)
     .then(response => response.data);
